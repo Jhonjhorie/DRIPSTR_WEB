34ebbbdecf04094f5d42f3a35b9ccf8e3cdf13b8
Inzagh1 <corporal.paolo.11162002@gmail.com>
2024-11-25 01:07:13 +0800

    sleep nako hihi

--- dripstr_web/src/modules/Shop_Profile/Component/Sidebars.js
+++ dripstr_web/src/modules/Shop_Profile/Component/Sidebars.js
@@ -6,44 +6,123 @@ function SideBar() {
   const [isNavbarVisible, setIsNavbarVisible] = useState(false);
   const navbarRef = useRef(null);
   const navigate = useNavigate();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  const toggleNavbar = () => {
-    setIsNavbarVisible(!isNavbarVisible);
-  };
-  
-    useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (navbarRef.current && !navbarRef.current.contains(event.target)) {
-        setIsNavbarVisible(false);
-      }
-    };
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-    }, []);
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+};
+
+ 
 
   return (
     <div className="relative flex  md:mr-0" ref={navbarRef}>
-    {/* Icon button */}
- 
+    
     <div className="dropdown dropdown-bottom dropdown-end bg-slate-100 shadow-md border-2 border-primary-color 
-    shadow-primary-color h-12 w-20 mt-2 rounded-md  ">
-      <div tabIndex={0} role="button" className="">
+    shadow-primary-color h-12 w-20 mt-2 rounded-md mr-16 ">
+      <div>
           <img
             src={logo}
             alt="Shop Logo"
             className="drop-shadow-custom object-cover rounded-md h-11 w-full"
           />
       </div>
-      <ul tabIndex={0} className="dropdown-content  h-72 bg-base-100 rounded-md z-[1] w-72 p-2 mt-2 shadow">
-        <div className='bg-slate-100 h-full w-full relative '>
-          <div className='bg-primary-color h-1/4 w-full'></div>
-          <div className='bg-base-100 h-[75%] w-full'>
-            <div className='text-white pt-[30%]  text-center'> Saint Mercy Apparel </div>
+    </div>
 
-            <div className='flex gap-2 mt-12'>
-              <button class="btn -ml-1 ">
+
+   
+    {/* Navbar */}
+    <button 
+      className="fixed w-12 h-12 right-3 z-10 bg-custom-purple  glass text-white rounded-md m-2 shadow-lg"
+      onClick={toggleSidebar}
+    >
+      <box-icon name='menu' color="#ffffff" ></box-icon>
+                    </button>
+                    {isSidebarOpen && (
+                        <div 
+                            className="fixed inset-0 bg-black opacity-50 z-40"
+                            onClick={toggleSidebar}
+                        ></div>
+                    )}
+      <div
+       className={`fixed top-0 right-0 w-72 h-full bg-base-100 shadow-lg transform transition-transform duration-300 z-50 ${isSidebarOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+
+        <ul className=" mt-16  md:mt-24 p-2">
+          <div className='h-24 w-full rounded-md bg-primary-color'>
+          <div className='bg-slate-100 absolute top-24 md:top-32 mx-[22%]  w-1/2  rounded-full border-[3px]  border-custom-purple ' >
+            <img
+              src={logo}
+              alt="Shop Logo"
+              className="drop-shadow-custom object-cover rounded-full h-full w-full"
+            />
+          </div>
+          <div className=' items-center justify-center h-10 w-10 absolute mx-2 place-self-end flex right-0'>
+            <box-icon type='solid' name='edit-alt'></box-icon>
+          </div>        
+          </div>
+
+          <div className='text-white pt-[30%]  text-center'> Saint Mercy Apparel </div>
+          <li
+          onClick={() => navigate('/shop/MerchantDashboard')}
+          className='flex justify-between p-1 hover:bg-base-200 rounded-md mt-5 hover:duration-200 hover:text-violet-900 cursor-pointer '>
+            <a
+            className="block  text-base text-gray-100 w-full hover:text-primary-color cursor-pointer ">
+              Dashboard
+            </a>
+            <box-icon type='solid' name='dashboard' color="#FFFFFF"></box-icon>
+          </li>
+
+          <li 
+          onClick={() => navigate('/shop/MerchantProducts')}
+          className='flex justify-between p-1 hover:bg-base-200 rounded-md hover:duration-200 hover:text-violet-900 cursor-pointer '>
+            <a
+            className="block  text-base text-gray-100 w-full hover:text-primary-color cursor-pointer ">
+              Products
+            </a>
+            <box-icon type='solid' name='component' color="#FFFFFF"></box-icon>
+          </li>
+
+          <li 
+          onClick={() => navigate('/shop/MerchantOrders')}
+          className='flex justify-between p-1 hover:bg-base-200 rounded-md hover:duration-200 hover:text-violet-900 cursor-pointer '>
+            <a
+             className="block  text-base text-gray-100 w-full hover:text-primary-color cursor-pointer ">
+              Orders
+            </a>
+            <box-icon name='basket' type='solid' color="#FFFFFF"></box-icon>
+          </li>
+
+          <li
+          onClick={() => navigate('/shop/MerchantNotifications')}
+          className='flex justify-between p-1 hover:bg-base-200 rounded-md hover:duration-200 hover:text-violet-900 cursor-pointer '>
+            <a
+            className="block  text-base text-gray-100 w-full hover:text-primary-color cursor-pointer ">
+              Notifications
+            </a>
+            <box-icon type='solid' name='bell' color="#FFFFFF"></box-icon>
+          </li>
+          
+          <li
+          onClick={() => navigate('/shop/MerchantFollowers')}
+          className='flex justify-between p-1 hover:bg-base-200 rounded-md hover:duration-200 hover:text-violet-900 cursor-pointer '>
+            <a   
+             className="block  text-base text-gray-100 w-full hover:text-primary-color cursor-pointer ">
+              Followers
+            </a>
+            <box-icon type='solid' name='group' color="#FFFFFF"></box-icon>
+          </li>
+
+          <li
+          onClick={() => navigate('/shop/Shop_profile')}
+          className='flex justify-between p-1 hover:bg-base-200 rounded-md hover:duration-200 hover:text-violet-900 cursor-pointer '>
+            <a   
+             className="block  text-base text-gray-100 w-full hover:text-primary-color cursor-pointer ">
+              Shop Preview
+            </a>
+            <box-icon type='solid' name='shopping-bags' color='#FFFFFF'></box-icon>
+          </li>
+        </ul>
+            <div className='flex gap-2 absolute bottom-14  md:bottom-2  justify-end '>
+              <button class="btn px-2 mx-2 ">
                 Messages
                 <div class="badge">+99</div>
               </button>
@@ -52,103 +131,8 @@ function SideBar() {
                 <div class="badge badge-secondary">+99</div>
               </button>
             </div>
-          </div>
-          <div className='bg-slate-100 absolute top-3 mx-[25%] h-1/2 w-1/2  rounded-full border-[3px]  border-custom-purple ' >
-            <img
-              src={logo}
-              alt="Shop Logo"
-              className="drop-shadow-custom object-cover rounded-full h-full w-full"
-            />
-          </div>
-         
-          
-          
-        </div>
-      </ul>
-    </div>
-
-    <button
-      className="w-10 h-10 flex items-center m-3 shadow-sm shadow-primary-color  justify-center
-       rounded-md"
-     
-    >
-      <label
-      
-      className="btn w-11 h-10  bg-primary-color glass hover:duration-300 hover:bg-slate-100 swap swap-rotate">
-        {/* this hidden checkbox controls the state */}
-        <input
-        onClick={toggleNavbar}
-        type="checkbox" />
-
-        {/* hamburger icon */}
-        <svg
-          className="swap-off fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          color='black'
-          viewBox="0 0 512 512">
-          <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
-        </svg>
-
-        {/* close icon */}
-        <svg
-          className="swap-on fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          color='black'
-          viewBox="0 0 512 512">
-          <polygon
-            points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
-        </svg>
-      </label>
-
-
-    </button>
-   
-    {/* Navbar */}
-    {isNavbarVisible && (
-      <div className="absolute top-16 right-0 w-48 mx-3 bg-slate-50 shadow-lg rounded-md p-4">
-        <ul className="space-y-2">
-          <li>
-            <a
-            onClick={() => navigate('/shop/MerchantDashboard')}
-            className="block text-base text-gray-800 hover:text-violet-900 cursor-pointer ">
-              Dashboard
-            </a>
-          </li>
-          <li>
-            <a
-            onClick={() => navigate('/shop/MerchantProducts')}
-            className="block text-base text-gray-800 hover:text-violet-900 cursor-pointer ">
-              Products
-            </a>
-          </li>
-          <li>
-            <a
-            onClick={() => navigate('/shop/MerchantOrders')}
-            className="block text-base text-gray-800 hover:text-violet-900 cursor-pointer ">
-              Orders
-            </a>
-          </li>
-          <li>
-            <a
-            onClick={() => navigate('/shop/MerchantNotifications')}
-            className="block text-base text-gray-800 hover:text-violet-900 cursor-pointer ">
-              Notifications
-            </a>
-          </li>
-          <li>
-            <a
-            onClick={() => navigate('/shop/MerchantFollowers')}
-            className="block text-base text-gray-800 hover:text-violet-900 cursor-pointer ">
-              Followers
-            </a>
-          </li>
-        </ul>
       </div>
-    )}
+    
   </div>
   );
 }
--- dripstr_web/src/modules/Shop_Profile/View/Products.js
+++ dripstr_web/src/modules/Shop_Profile/View/Products.js
@@ -6,7 +6,7 @@ function Products() {
   
   return (
   <div className="h-full w-full overflow-y-scroll bg-slate-300 custom-scrollbar  md:flex">
-    <div className='absolute mx-3 right-0'>
+    <div className='absolute mx-3 right-0 z-10'>
       <SideBar/>
     </div>
     <div className='w-full md:w-1/2 h-full bg-slate-500'>
