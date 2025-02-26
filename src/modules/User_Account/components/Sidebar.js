ff599d9788d5d5578819e5b6bddc576c8782f097
Jhonjhorie <74289488+Jhonjhorie@users.noreply.github.com>
2025-02-26 13:36:07 +0800

    Update Sidebar.js

--- src/modules/User_Account/components/Sidebar.js
+++ src/modules/User_Account/components/Sidebar.js
@@ -25,7 +25,7 @@ const navItems = [
   { label: "My Order", path: "/account/orders", icon: faReceipt },
   //{ label: "Followed Creators", path: "/account/wishlist", icon: faHeart },
   { label: "Set up Shop", path: "/account/shop-setup", icon: faStore },
-  //{ label: "Avatar", path: "/account/avatar", icon: faUserCircle },
+  { label: "Avatar", path: "/account/avatar", icon: faUserCircle },
 ];
 
 const AccountLayout = () => {
