0f599614ab94e816f469410e89848757cf1a80da
123Sora123 <168890845+123Sora123@users.noreply.github.com>
2025-03-19 04:51:43 +0800

    wwwss

---
+++ src/constants/avatarConfig.js
@@ -0,0 +1,73 @@
+// src/constants/avatarConfig.js
+
+const bodyTypeURLs = {
+    Boy: {
+      Average: "/3d/avatars/guyz/Average.glb",
+     // Broad: "/3d/avatars/guyz/Heavy.glb",
+      Muscular: "/3d/avatars/guyz/Muscular.glb",
+      PlusSize: "/3d/avatars/guyz/PlusSize.glb",
+      Petite: "/3d/avatars/guyz/Thin.glb",
+     // Dagul: "/3d/avatars/guyz/Dagul.glb",
+     Dance: "/3d/avatars/guyz/dance-transform.glb",
+    },
+    Girl: {
+      Average: "/3d/avatars/gurlz/Average.glb",
+      Muscular: "/3d/avatars/gurlz/Muscular.glb",
+      PlusSize: "/3d/avatars/gurlz/PlusSize.glb",
+      Petite: "/3d/avatars/gurlz/Thin.glb",
+    },
+  };
+  
+  const hairURLs = {
+    Barbers: "/3d/hair/man/ManHair1.glb",
+    PogiCut: "/3d/hair/man/ManHair2.glb",
+    DoublePonytail: "/3d/hair/woman/GirlHair1.glb",
+    Short: "/3d/hair/woman/GirlHair2.glb",
+    Kulot: "/3d/hair/woman/GirlHair3.glb",
+  };
+
+  const tshirURLs = {
+    Boy:{
+      //Average: "/3d/wears/guyz/tshirts/Average_Tshirt.glb",
+      Average: "/3d/uvmap/average.glb",
+      //Average: "/3d/uvmap/untitled.glb",
+
+      Muscular: "/3d/wears/guyz/tshirts/Muscular_Tshirt.glb",
+      Petite: "/3d/wears/guyz/tshirts/Petite_Tshirt.glb",
+      PlusSize: "/3d/wears/guyz/tshirts/PlusSize_Tshirt.glb",
+    },
+    Girl:{
+      Average: "/3d/wears/gurlz/tshirts/Average_Tshirt.glb",
+      Muscular: "/3d/wears/gurlz/tshirts/Muscular_Tshirt.glb",
+      Petite: "/3d/wears/gurlz/tshirts/Petite_Tshirt.glb",
+      PlusSize: "/3d/wears/gurlz/tshirts/PlusSize_Tshirt.glb",
+    }
+  }
+
+  const shortsURLs = {
+    Boy:{
+      Average: "/3d/wears/guyz/shorts/Average_Short.glb",
+      Muscular: "/3d/wears/guyz/shorts/Muscular_Short.glb",
+      Petite: "/3d/wears/guyz/shorts/Petite_Short.glb",
+      PlusSize: "/3d/wears/guyz/shorts/PlusSize_Short.glb",
+    },
+    Girl:{
+      Average: "/3d/wears/gurlz/shorts/Average_Short.glb",
+      Muscular: "/3d/wears/gurlz/shorts/Muscular_Short.glb",
+      Petite: "/3d/wears/gurlz/shorts/Petite_Short.glb",
+      PlusSize: "/3d/wears/gurlz/shorts/PlusSize_Short.glb",
+    }
+  }
+  
+const tshirtTextureURLs = {
+  Boy: {
+    Average: "/3d/uvmap/TexturedMESH.png",
+  },
+  Girl: {
+    Average: "/3d/wears/guyz/tshirts/Average_Tshirt_Texture.glb",
+  }
+
+}
+
+  export { bodyTypeURLs, hairURLs, tshirURLs, shortsURLs, tshirtTextureURLs};
+  
